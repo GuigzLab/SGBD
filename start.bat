ef8e290e746dee391f9e8de8535f0e113f1f4e41
GuigzPC <guillaume.dufau.pro@gmail.com>
2020-10-19 13:32:22 +0200

    Launcher

--- start.bat
+++ start.bat
@@ -1,2 +1,4 @@
-javac Code\src\up\mi\sgbdr\Main.java
-java Code\src\up\mi\sgbdr\Main
+@echo off
+javac -d Code\out\production\Code\ Code\src\up\mi\sgbdr\*.java
+java -cp Code/out/production/Code/src/ up/mi/sgbdr/Main
+pause
