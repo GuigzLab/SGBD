2bb503bc247c4ed7b814c1818ba21aae6a37856e
GuigzPC <guillaume.dufau.pro@gmail.com>
2020-12-15 22:09:02 +0100

    update architecture

--- start_windows.bat
+++ start_windows.bat
@@ -1,4 +1,4 @@
 @echo on
-javac -d Code/out/production/Code/ Code/src/up/mi/sgbdr/*.java
-java -cp ./Code/out/production/Code/ up.mi.sgbdr.Main
+javac -d Code/out/production/ Code/src/up/mi/sgbdr/*.java
+java -cp ./Code/out/production/ up.mi.sgbdr.Main
 pause
